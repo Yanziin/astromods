a25cfbd219b62901fbbfa549e906a6c9afaa662e
psichott <93738038+Yanziin@users.noreply.github.com>
2024-10-25 08:01:49 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+app = Flask(__name__)
+import random
+import os
+
+@app.route('/')
+def hello_world():
+    if 'PORT' in os.environ:
+        return 'tem porta'
+    return 'no port'
